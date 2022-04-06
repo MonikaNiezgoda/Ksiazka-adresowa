a51cab12568d149a31424a9a7a9e9db959fb20e7
MonikaNiezgoda <m.niezgoda.programista@gmail.com>
2022-04-06 20:19:34 +0200

    Added class Adresat

---
+++ Adresat.h
@@ -0,0 +1,24 @@
+#ifndef ADRESAT_H
+#define ADRESAT_H
+#include <iostream>
+
+using namespace std;
+
+class Adresat
+{
+    int id, int idUzytkownika;
+    string login;
+    string haslo;
+    string numerTelefonu, string email, string adres;
+
+/*public:
+    void ustawId(int noweId);
+    void ustawLogin(string nowyLogin);
+    void ustawHaslo(string noweHaslo);
+
+    int pobierzId();
+    string pobierzLogin();
+    string pobierzHaslo();*/
+};
+
+#endif
